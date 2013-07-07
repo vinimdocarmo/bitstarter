eade333a48ef07e98a315f38f38196eba78467bc
Vinicius do Carmo <vinicius.m.do.carmo@gmail.com>
2013-07-07 20:18:56 -0300

    Update web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var express = require('express'),
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('hello node.js');
+  response.send(fs.readFileSync('index.html'));
 });
 
 var port = process.env.PORT || 5000;
